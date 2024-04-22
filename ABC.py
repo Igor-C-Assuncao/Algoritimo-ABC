ecff2fc0314b096cdf8077280046ac3f913224a6
igor-c-assuncao <igor.00000829605@unicap.br>
2024-04-22 15:51:45 -0300

    Done

--- ABC.py
+++ ABC.py
@@ -1,37 +1,62 @@
 import numpy as np
 
 class ABC:
-    def __init__(self, func, n_agents, n_iter, dim, limit):
-        self.func = func
-        self.n_agents = n_agents
-        self.n_iter = n_iter
-        self.dim = dim
-        self.limit = limit
+    def __init__(self, problem, population_size, iterations):
+        self.problem = problem
+        self.population_size = population_size
+        self.iterations = iterations
+
+    def initialization_phase(self):
+        population = [self.problem.random_solution() for _ in range(self.population_size)]
+        return population
+
+    def employed_bees_phase(self, population):
+        for i, solution in enumerate(population):
+            neighbor_index = np.random.choice(range(self.population_size))
+            parameter_index = np.random.randint(self.problem.dimension)
+            phi = np.random.uniform(-1, 1)
+            neighbor_solution = solution + phi * (solution - population[neighbor_index])
+            neighbor_solution = np.clip(neighbor_solution, self.problem.lower_bound, self.problem.upper_bound)
+            neighbor_fitness = self.problem.objective_function(neighbor_solution)
+
+            if neighbor_fitness < self.problem.objective_function(solution):
+                population[i] = neighbor_solution
+
+        return population
+
+    def onlooker_bees_phase(self, population):
+        fitness_values = [self.problem.objective_function(solution) for solution in population]
+        probabilities = [fit / sum(fitness_values) for fit in fitness_values]
+
+        onlooker_population = []
+        for _ in range(self.population_size):
+            selected_index = np.random.choice(range(self.population_size), p=probabilities)
+            onlooker_population.append(population[selected_index])
+
+        return onlooker_population
+
+    def scout_bees_phase(self, population):
+        for i, solution in enumerate(population):
+            if np.random.rand() < 0.1:  # Probability threshold for abandoning the solution
+                population[i] = self.problem.random_solution()
+
+        return population
 
     def optimize(self):
+        population = self.initialization_phase()
         best_solution = None
         best_fitness = np.inf
-        agents = np.random.uniform(low=-self.limit, high=self.limit, size=(self.n_agents, self.dim))
 
-        for _ in range(self.n_iter):
-            for i, agent in enumerate(agents):
-                new_agent = self.explore(agent, agents)
-                fitness = self.func(new_agent)
-                if fitness < best_fitness:
-                    best_fitness = fitness
-                    best_solution = new_agent.copy()
-                if fitness < self.func(agent):
-                    agents[i] = new_agent.copy()
-        
+        for _ in range(self.iterations):
+            employed_population = self.employed_bees_phase(population)
+            onlooker_population = self.onlooker_bees_phase(employed_population)
+            population = self.scout_bees_phase(onlooker_population)
+
+            current_best_fitness = min([self.problem.objective_function(sol) for sol in population])
+            if current_best_fitness < best_fitness:
+                best_solution = population[np.argmin([self.problem.objective_function(sol) for sol in population])]
+                best_fitness = current_best_fitness
+
         return best_solution, best_fitness
-
-    def explore(self, agent, agents):
-        selected_agents = agents[np.random.choice(len(agents), 3, replace=False)]
-        phi = np.random.uniform(low=-1, high=1, size=self.dim)
-        new_agent = agent + phi * (agent - selected_agents.mean(axis=0))
-        new_agent = np.clip(new_agent, -self.limit, self.limit)
-        return new_agent
-
-
 
 
--- Otimizacao.py
+++ Otimizacao.py
@@ -1,35 +1,39 @@
 import numpy as np
 
+class OptimizationProblem:
+    def __init__(self, dimension, lower_bound, upper_bound):
+        self.dimension = dimension
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+
+    def objective_function(self, x):
+        raise NotImplementedError("Subclasses must implement objective_function method")
+
+    def random_solution(self):
+        return self.lower_bound + np.random.rand(self.dimension) * (self.upper_bound - self.lower_bound)
 
 
-class Rastrigin:
-    def __init__(self, dim):
-        self.dim = dim
 
-    def evaluate(self, x):
-        return np.sum(x**2 - 10 * np.cos(2 * np.pi * x) + 10)
-
-
-class Rosenbrock:
-    def __init__(self, dim):
-        self.dim = dim
-
-    def evaluate(self, x):
-        return np.sum(100*(x[1:] - x[:-1]**2)**2 + (x[:-1] - 1)**2)
+class Rastrigin(OptimizationProblem):
+    def objective_function(self, x):
+        A = 10
+        return A * self.dimension + np.sum(x**2 - A * np.cos(2 * np.pi * x))
 
 
-class Ackley:
-    def __init__(self, dim):
-        self.dim = dim
 
-    def evaluate(self, x):
-        return -20 * np.exp(-0.2 * np.sqrt(np.sum(x**2) / self.dim)) - \
-               np.exp(np.sum(np.cos(2 * np.pi * x)) / self.dim) + 20 + np.exp(1)
+class Rosenbrock(OptimizationProblem):
+    def objective_function(self, x):
+        return np.sum(100 * (x[1:] - x[:-1]**2)**2 + (1 - x[:-1])**2)
 
 
-class Esfera:
-    def __init__(self, dim):
-        self.dim = dim
 
-    def evaluate(self, x):
+class Ackley(OptimizationProblem):
+    def objective_function(self, x):
+        a = 20
+        b = 0.2
+        c = 2 * np.pi
+        return -20 * np.exp(-b * np.sqrt(np.sum(x**2) / self.dimension)) - np.exp(np.sum(np.cos(c * x)) / self.dimension) + a + np.exp(1)
+
+class Sphere(OptimizationProblem):
+    def objective_function(self, x):
         return np.sum(x**2)
